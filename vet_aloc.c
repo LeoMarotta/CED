f3bf3465547db431e42b5cac0a3c0098d746fcde
Leonardo Marotta <sen.aka@hotmail.com>
2020-11-12 11:22:10 -0300

    aloc

--- vet_aloc.c
+++ vet_aloc.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    
+
+    /*
+
+    //VETORES
+
     int v[10];
 
     for(int i=0; i<10; i++){
         v[i] = i;
         printf("%d\n", v[i]);
     }
+    */
+
+    /*
+    //ALOCAÇÃO
+
+    int *v;
+
+    v = (int*)malloc(10 * sizeof(int));
+    if(v == NULL){
+        printf("Memoria insuficiente");
+        exit(1);
+    }
+
+    v[0] = 10;
+    v[1] = 20;
+
+    printf("%d\n", v[0]);
+
+    free(v);
+    */
+
 
 
     return 0;

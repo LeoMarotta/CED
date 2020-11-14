9215962d1c93b102f35ed1a87b6499d56fa2e393
Leonardo Marotta <sen.aka@hotmail.com>
2020-11-14 18:38:40 -0300

    matriz

--- matrizes.c
+++ matrizes.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    float mat[2][2] = {{2,3},{4,5}};
-
+void f(float mat[2][2]){
     for(int i=0;i<2;i++){
         for(int j=0;j<2;j++){
             printf("%.2f\t",mat[i][j]);
         }
         printf("\n");
     }
+}
+
+int main(){
+    float mat[2][2] = {{2,3},{4,5}};
+
+    f(mat);
 
     return 0;
 }

7a9476b8b8f7c8431386d96ae93b1b9581ee23eb
Leonardo Marotta <sen.aka@hotmail.com>
2020-11-14 14:17:02 -0300

    matriz

--- matrizes.c
+++ matrizes.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 
 int main(){
-    float mat[2][2] = {{2,3},{4,5},{6,7},{8,9}};
+    float mat[2][2] = {{2,3},{4,5}};
+
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            printf("%.2f\t",mat[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
